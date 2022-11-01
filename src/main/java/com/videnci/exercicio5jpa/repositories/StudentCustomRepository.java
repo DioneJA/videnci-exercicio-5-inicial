2d57614236d25e59ae586c45a220a331b81975b7
DioneJA <brunoalves2017@gmail.com>
2022-11-01 16:12:31 -0300

    UPDATED | Back End impl

--- src/main/java/com/videnci/exercicio5jpa/repositories/StudentCustomRepository.java
+++ src/main/java/com/videnci/exercicio5jpa/repositories/StudentCustomRepository.java
@@ -9,6 +9,7 @@ import javax.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
 
 import com.videnci.exercicio5jpa.entities.Student;
+import com.videnci.exercicio5jpa.services.exceptions.ResourceNotFoundException;
 
 @Repository
 public class StudentCustomRepository {
@@ -20,21 +21,26 @@ public class StudentCustomRepository {
 
 	@SuppressWarnings("unchecked") /* Warning ignorado */
 	public Student findStudent(Student students) {
-		List<Object[]> assistent;
-		String query = "SELECT tb_student.id, max(test_note), "
-				+ "min(test_note), AVG(test_note), "
-				+ "COUNT(test_note) FROM "
-				+ "tb_student JOIN tb_test ON tb_student.id = tb_test.id_student GROUP BY tb_student.id";
-		var q = entityManager.createNativeQuery(query);
-		assistent = (List<Object[]>) q.getResultList();
-		for (int i = 0; i < assistent.size(); i++) {
-			if (((BigInteger) assistent.get(i)[0]).longValue() == students.getId()) {
-				students.setHighestGrade((Double) assistent.get(i)[1]);
-				students.setLowestGrade((Double) assistent.get(i)[2]);
-				students.setAverage((Double) assistent.get(i)[3]);
-				students.setAmountTests(((BigInteger) assistent.get(i)[4]).intValue());
+		try {
+			List<Object[]> assistent;
+			String query = "SELECT tb_student.id, max(test_note), "
+					+ "min(test_note), AVG(test_note), "
+					+ "COUNT(test_note) FROM "
+					+ "tb_student JOIN tb_test ON tb_student.id = tb_test.id_student GROUP BY tb_student.id";
+			var q = entityManager.createNativeQuery(query);
+			assistent = (List<Object[]>) q.getResultList();
+			for (int i = 0; i < assistent.size(); i++) {
+				if (((BigInteger) assistent.get(i)[0]).longValue() == students.getId()) {
+					students.setHighestGrade((Double) assistent.get(i)[1]);
+					students.setLowestGrade((Double) assistent.get(i)[2]);
+					students.setAverage((Double) assistent.get(i)[3]);
+					students.setAmountTests(((BigInteger) assistent.get(i)[4]).intValue());
+				}
 			}
+			return students;
+		}catch (Exception e) {
+			throw new ResourceNotFoundException(students.getId());
 		}
-		return students;
+
 	}
 }
