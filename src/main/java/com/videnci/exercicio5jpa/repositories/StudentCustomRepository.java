3278b94c938af5a101096bbdd920540f3a2a72e3
DioneJA <brunoalves2017@gmail.com>
2022-11-01 15:51:28 -0300

    SQL fix

--- src/main/java/com/videnci/exercicio5jpa/repositories/StudentCustomRepository.java
+++ src/main/java/com/videnci/exercicio5jpa/repositories/StudentCustomRepository.java
@@ -21,9 +21,9 @@ public class StudentCustomRepository {
 	@SuppressWarnings("unchecked") /* Warning ignorado */
 	public Student findStudent(Student students) {
 		List<Object[]> assistent;
-		String query = "SELECT tb_student.id,max(test_note) AS `highestGrade`,"
-				+ "min(test_note) AS `lowestGrade`,AVG(test_note) AS `average`, "
-				+ "COUNT(test_note) AS `amountTests` FROM "
+		String query = "SELECT tb_student.id, max(test_note), "
+				+ "min(test_note), AVG(test_note), "
+				+ "COUNT(test_note) FROM "
 				+ "tb_student JOIN tb_test ON tb_student.id = tb_test.id_student GROUP BY tb_student.id";
 		var q = entityManager.createNativeQuery(query);
 		assistent = (List<Object[]>) q.getResultList();
@@ -31,7 +31,7 @@ public class StudentCustomRepository {
 			if (((BigInteger) assistent.get(i)[0]).longValue() == students.getId()) {
 				students.setHighestGrade((Double) assistent.get(i)[1]);
 				students.setLowestGrade((Double) assistent.get(i)[2]);
-				students.setAverage(((BigDecimal) assistent.get(i)[3]).doubleValue());
+				students.setAverage((Double) assistent.get(i)[3]);
 				students.setAmountTests(((BigInteger) assistent.get(i)[4]).intValue());
 			}
 		}
