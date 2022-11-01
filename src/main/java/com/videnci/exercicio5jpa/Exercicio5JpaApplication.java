d28df0aaa586bf251a44157496f8297df50bf76c
DioneJA <brunoalves2017@gmail.com>
2022-11-01 15:37:35 -0300

    Updated

--- src/main/java/com/videnci/exercicio5jpa/Exercicio5JpaApplication.java
+++ src/main/java/com/videnci/exercicio5jpa/Exercicio5JpaApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Exercicio5JpaApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("server.port", "8081");
 		SpringApplication.run(Exercicio5JpaApplication.class, args);
 	}
 
